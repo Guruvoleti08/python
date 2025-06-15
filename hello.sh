0bf27e7580e693060b2d31ba22bd1c114f3a68e3
Guruvoleti08 <voletiguru3@gmail.com>
2025-06-15 13:51:59 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,17 +1,4 @@
- #!/bin/bash
- set -x
+#!/bin/bash
 
-# Initialize variables
-count=1
-rows=4  # Number of rows (adjust as needed)
-
-# Loop to print numbers in triangle format
-for (( i=1; count<=10 && i<=rows; i++ ))
-do
-  for (( j=1; j<=i && count<=10; j++ ))
-  do
-    echo -n "$count "
-    ((count++))
-  done
-  echo
-done
+echo "Hello World"
+echo "---------------------------------"
